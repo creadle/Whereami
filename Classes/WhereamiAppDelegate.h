7198a18ed2a22faee30fe175c07a9216a4539507
Chris Readle <patterner@rocketmail.com>
2010-09-17 13:53:04 -0600

    First commit for Whereami app from the big nerd ranch book

---
+++ Classes/WhereamiAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  WhereamiAppDelegate.h
+//  Whereami
+//
+//  Created by Chris Readle (GMC-MSV-IT CONTRACTOR) on 9/17/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WhereamiAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
